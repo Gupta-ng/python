3e1aeeb9c26bc9206d1719beb2869c081cdfe3e2
Gupta-ng <122103488+Gupta-ng@users.noreply.github.com>
2023-03-05 13:49:43 +0530

    Add files via upload

---
+++ floor.py
@@ -0,0 +1,8 @@
+L=int(input("length of the rectangular floor->"))
+B=int(input("width of the rectangular floor->"))
+l=int(input("length of the tiles->"))
+b=int(input("width of the tiles->"))
+A=L*B
+a=l*b
+Tiles=A/a
+print("no of tiles required->",Tiles)
